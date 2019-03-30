ff7aaef2e363bf47f8db0dce88175a86e5a6e89b
yeah-right <claar.zack@gmail.com>
2019-03-30 11:31:25 -0600

    tests passing

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,58 @@
-# Write your code here.
+# # Write your code here.
+# def line(katz_deli)
+#   if katz_deli.length == 0
+#     puts "The line is currently empty."
+#   else
+#     current_line = "The line is currently: "
+#     deli.each_with_index(1) do |person, index|
+#       current_line << "#{index}. #{person}"
+#     end
+#     puts current_line
+#   end
+# end
+#
+# def take_a_number(katz_deli, customer_name)
+#   katz_deli << customer_name
+#   puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+# end
+#
+# def now_serving(katz_deli)
+#   if katz_deli.empty?
+#     puts "There is nobody waiting to be served!"
+#   else
+#     puts "Currently serving #{katz_deli.first}"
+#     katz_deli.shift
+#   end
+# end
+# # def take_a_number(katz_deli, new_customer)
+# #   new_customer << katz_deli
+# #   queue_message = "The line is currently: "
+# #   katz_deli.each do |customer, position|
+# #     queue_message + "#{position}. "
+# #   end
+# # end
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.first}."
+    deli.shift
+  end
+end
